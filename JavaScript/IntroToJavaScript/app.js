480fdbea28fadafdc2b4db75c9cdfa307ef0482f
drumkingcaleb <30183747+drumkingcaleb@users.noreply.github.com>
2018-10-28 11:02:32 -0600

    added added the IntroToJavaScript folder

---
+++ JavaScript/IntroToJavaScript/app.js
@@ -0,0 +1,20 @@
+console.log(18);
+console.log(260);
+
+//opening line
+/*
+Caleb Stamper
+CS Special Topics
+Introduction to JavaScript
+16 October 2018
+*/
+
+console.log(17+3.5);
+console.log(2018-1969);
+console.log(.2708*100);
+
+console.log('hello'+'world');
+console.log('hello '+'world');
+
+console.log('Teaching the world how to code'.length);
+console.log('Codeacademy'.toUpperCase());
